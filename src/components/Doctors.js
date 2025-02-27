ffcc2ab93fb3a9631820ec028993bff1075b0674
nehad222 <nihadshrith@gmail.com>
2025-02-27 12:28:05 +0000

    asnfa

--- src/components/Doctors.js
+++ src/components/Doctors.js
@@ -6,16 +6,16 @@ const Doctors_Students = (props) => {
     <div className='catges'>
       {props.items.map((catg,catgId)=>{
         return(
-            <div key={catgId}>
+            <div key={catgId} >
                 
                 {props.doctor_student == 1 && catg.limited_doctor_items?.length > 0 ? (
-                    <div >
-                        <h1 className='catg_Name'>{catg.name}</h1>
+                    <div className='catg'>
+                        <h1 className='catg_Name '>{catg.name}</h1>
                         <h3 className='catg_Des' >{catg.description}</h3>
                         <Slider items={catg.limited_doctor_items} />
                     </div>
 ) : props.doctor_student == 2 && catg.limited_student_items?.length > 0 ? (
-    <div>
+    <div className='catg'>
     <h1 className='catg_Name'>{catg.name}</h1>
     <h3 className='catg_Des' >{catg.description}</h3>
     <Slider items={catg.limited_student_items} />
