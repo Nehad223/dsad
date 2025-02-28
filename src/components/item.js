9365e25cf93a47cbb59141a6ebe3cd7cedf7815a
nehad222 <nihadshrith@gmail.com>
2025-02-28 16:22:26 +0000

    dasdas

--- src/components/item.js
+++ src/components/item.js
@@ -3,7 +3,7 @@ import item_Img from './Assests/item.png'
 const Item = (props) => {
   return (
     <div className='Item mb-4 '>
-        <img className='Item_Image'    src={`https://res.cloudinary.com/dgocqho3b/${props.item.photo}`} referrerPolicy="no-referrer" />
+        <img className='Item_Image'    src={`https://res.cloudinary.com/dgocqho3b/${props.item.photo}`} referrerPolicy="no-referrer"   loading="lazy"/>
         <div className='Item_inf'>
             <h5 className='Item_Name'>{props.item.name}</h5>
             <p className='Item_Price'>{props.item.price.toLocaleString()}</p>
