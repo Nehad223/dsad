999eded63dfc9f015bab6f7ff846a4970bb4f459
nehad222 <nihadshrith@gmail.com>
2025-02-28 13:28:40 +0000

    adsnas

--- src/components/Item_Package.js
+++ src/components/Item_Package.js
@@ -3,7 +3,7 @@ import item_Img from './Assests/item.png';
 const Item_Package = (props) => {
   return (
     <div className='Item_Package'>
-      <img  src={item_Img} className='Item_Image' />
+      <img  src={`https://res.cloudinary.com/dgocqho3b/${props.items.photo}`} className='Item_Image' />
       <div className='Item_Info_Pack'>
         <div className='Item_Price_Pack_Box' ><h5 className='Item_Price_Pack'>{props.items.price}</h5></div>
         
