4a332aed427ffcd22705cd3d84977de8cf2ccbc7
nehad222 <nihadshrith@gmail.com>
2025-03-03 16:28:03 +0300

    danda

--- src/components/item.js
+++ src/components/item.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React from 'react';
+
+import LazyImage from './LazyImage'; // استيراد LazyImage
+
 const Item = (props) => {
+
   return (
-    <div className='Item mb-4 '>
-        <img className='Item_Image'    src={`https://res.cloudinary.com/dgocqho3b/${props.item.photo}`} referrerPolicy="no-referrer"   loading="lazy"/>
-        <div className='Item_inf'>
-            <h5 className='Item_Name'>{props.item.name}</h5>
-            <p className='Item_Price'>{props.item.price.toLocaleString()}</p>
-        </div>
+    <div className='Item mb-4'>
+      <LazyImage
+        className='Item_Image'
+        src={`https://res.cloudinary.com/dgocqho3b/${props.item.photo}`}
+        alt={props.item.name}
+        placeholder="https://via.placeholder.com/150" // Placeholder image before the actual image is loaded
+      />
+      <div className='Item_inf'>
+        <h5 className='Item_Name'>{props.item.name}</h5>
+        <p className='Item_Price'>{props.item.price.toLocaleString()}</p>
+      </div>
+      {/* Commented out button */}
     </div>
-  )
+  );
 }
 
 export default Item;
-
