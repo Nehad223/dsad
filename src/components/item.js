f126011b23b510f9e664c937fae87211a0221ebb
nehad222 <nihadshrith@gmail.com>
2025-03-03 16:34:44 +0300

    dasdasd

--- src/components/item.js
+++ src/components/item.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import logo from './Assests/logo.png'
 import LazyImage from './LazyImage'; // استيراد LazyImage
 
 const Item = (props) => {
@@ -10,7 +10,7 @@ const Item = (props) => {
         className='Item_Image'
         src={`https://res.cloudinary.com/dgocqho3b/${props.item.photo}`}
         alt={props.item.name}
-        placeholder="https://via.placeholder.com/150" // Placeholder image before the actual image is loaded
+        placeholder={logo} // Placeholder image before the actual image is loaded
       />
       <div className='Item_inf'>
         <h5 className='Item_Name'>{props.item.name}</h5>
