ccc6b7086fc3802bf82f88cdddb1ea0c110879af
nehad222 <nihadshrith@gmail.com>
2025-03-20 22:45:17 +0300

    dsadasasd

--- src/App2.js
+++ src/App2.js
@@ -3,14 +3,12 @@ import './App.css';
 import { createContext } from 'react';
 import Catg from './components/Catg';
 import { useState } from 'react';
+import './components/All.css';
 import { Route,Routes } from 'react-router-dom';
 import Test from './components/Test';
 import Start_Page from './components/Start_Page';
 import Conformation_Page from './components/Conformation_Page';
 import Home_Page from './components/Home_Page';
-import ProductSlider from './components/TestSwiper';
-import ParentComponent from './components/TestButton';
-import Test_Col from './components/Test_Col';
 import Search_Page from './components/Search_Page';
 import Cart from './components/Cart';
 import Points_Page from './components/Points_Page';
@@ -30,7 +28,7 @@ function App() {
         <Route path='/dsad/Test' element={<Test/>} />
         <Route path='/dsad/search' element={<Search_Page/>} />
         <Route path='/dsad/cart' element={<Cart/>} />
-        <Route path='/dsad/profile/' element={<Profile/>} />
+        <Route path='/dsad/profile' element={<Profile/>} />
         <Route path='/dsad/profile/points' element={<Points_Page/>} />
       </Routes>
     </div>
