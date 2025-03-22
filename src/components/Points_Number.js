68cc588777304bdeb73992102037afd793163e7b
nehad222 <nihadshrith@gmail.com>
2025-03-22 17:39:53 +0300

    dsvfevev

--- src/components/Points_Number.js
+++ src/components/Points_Number.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Points_Number = (props) => {
   return (
     <div>
-        <button className='points_btn mb-5'>
+        <button className='points_btn mb-2'>
             <div className="num_points">{props.points}</div>
             <div className="name_points">عدد النقاط</div>
         </button>
