b2aec798d9e4b6075b87cea77065b39e4c50b788
nehad222 <nihadshrith@gmail.com>
2025-03-23 15:19:10 +0300

    sderwg

--- src/Pages/Buy_Points.js
+++ src/Pages/Buy_Points.js
@@ -4,12 +4,13 @@ import Points_Number from "../components/Points_Number";
 import { useEffect } from 'react';
 import Form from '../components/Form';
 import { useLocation } from 'react-router-dom';
-
+import TelegramBackButton from '../components/Tele_Back_Btn';
 const Buy_Points = () => {
   const location = useLocation();
   const name = location.state?.name || "";
   const points = location.state?.points || "";
   const photo = location.state?.photo || "";
+  TelegramBackButton();
 
   useEffect(() => {
     document.documentElement.style.setProperty("--main", "white");
