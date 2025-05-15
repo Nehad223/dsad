15feb587b1b434dc0f50bbd636e40c46e357c629
nehad222 <nihadshrith@gmail.com>
2025-05-15 18:47:12 +0300

    asfwr

--- src/Pages/Conformation_Page.js
+++ src/Pages/Conformation_Page.js
@@ -16,9 +16,9 @@ const Conformation_Page = () => {
     <div className="container Conformation_Page">
       <div className="row justify-content-center">
         <div className="col-12 d-flex justify-content-center">
-          <img src={mobile} className="Mobile_Img " />
+          <img src={mobile} className="Mobile_Img" />
         </div>
-        <div className="col-12  text-center Card_Tag">
+        <div className="col-12 text-center Card_Tag">
           <h1 className="Card_Title">Dental.kit أهلا بك في</h1>
           <p className="Card_Par">
             Lorem ipsum is simply dummy text of the printing <br />
