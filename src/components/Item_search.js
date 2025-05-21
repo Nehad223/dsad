8ab2a0dc1237befe318f54978bede453e100d7a1
nehad222 <nihadshrith@gmail.com>
2025-05-21 10:02:33 +0300

    asnfaf

--- src/components/Item_search.js
+++ src/components/Item_search.js
@@ -7,11 +7,11 @@ const Go_To_Item=()=>{
   return (
     <div onClick={Go_To_Item}>
           <div className="Item_Package" >
-            {props.item.photo && (      <img
+                 <img
         src={`https://res.cloudinary.com/dgocqho3b/${props.items.photo}`}
         className="Item_Image"
-      />)}
-  {props.item.photo && (
+      />
+
       <div className="Item_Info_Pack">
         <div className="Item_Price_Pack_Box">    
             {props.currency=="sp"? <h5 className="Item_Price_Pack">
@@ -25,7 +25,7 @@ const Go_To_Item=()=>{
         </div>
 
         <h1 className="Item_Pack_Name">{props.items.name}</h1>
-      </div>)}
+      </div>
 
 
     </div>
--- src/components/in_Buy_Item.js
+++ src/components/in_Buy_Item.js
@@ -4,37 +4,45 @@ import { useCart } from '../context/CartContext';
 import { useParams } from 'react-router-dom';
 import Cart from './Cart_Point'
 import Price_Btn from './Price_Btn';
+
 const In_Buy_Item = (props) => {
-  const parmas=useParams();
-  const type=parmas.itemORpackage;
+  const parmas = useParams();
+  const type = parmas.itemORpackage;
   const { addToCart } = useCart();
   const [quantity, setQuantity] = useState(1);
+
   const confirmAdd = () => {
-    addToCart(props.item,quantity,type);
-    console.log(props.item,quantity,type)
+    addToCart(props.item, quantity, type);
+    console.log(props.item, quantity, type);
   };
 
   return (
     <div className='in_Buy_Item'>
       <Price_Btn title={props.item.name} points={props.item.price} items={true} />
-      {type=="packages"?
-      <div> <h1 className='mt-4 Name_Buy'>بكج {props.item.name}</h1>
-      <h1 className='Contents_Packages'>المحتويات</h1>
-      </div>
-      :
-       <h1 className='mt-4 Name_Buy'>{props.item.name}</h1>}
-      <p className='mt-5 kk'>{props.item.description}</p>
+      
+      {type === "packages" ? (
+        <div>
+          <h1 className='mt-4 Name_Buy'>بكج {props.item.name}</h1>
+          <h1 className='Contents_Packages'>المحتويات</h1>
+        </div>
+      ) : (
+        <h1 className='mt-4 Name_Buy'>{props.item.name}</h1>
+      )}
+
+      <p className='kk'>{props.item.description}</p>
+
+      {/* Spacer ليدفش الزر لتحت */}
+      <div className="spacer"></div>
+
       <div className='btns_buys'>
-            <Cart quantity={quantity} setQuantity={setQuantity} />
+        <Cart quantity={quantity} setQuantity={setQuantity} />
         <button className='confirm_add' onClick={confirmAdd}>
           أضف للسلة
         </button>
       </div>
-
-      </div>
+    </div>
   );
 };
 
 export default In_Buy_Item;
-
 
