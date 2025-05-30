a802f275f3b751086ba48c37c3fea8193420492c
nehad222 <nihadshrith@gmail.com>
2025-05-30 14:12:39 +0300

    gdggdg

--- src/components/Form.js
+++ src/components/Form.js
@@ -80,8 +80,8 @@ const Form = (props) => {
       toast.success("تم ارسال الطلب بنجاح");
       setConfirmStage(false);
       setTimeout(() => {
-        navigate('/dsad/profile');
-      }, 1500);
+        navigate('/dsad/home');
+      }, 100);
     } catch (error) {
       console.error(error);
       toast.error("حصل خطأ أثناء ارسال الطلب، الرجاء المحاولة لاحقاً");
